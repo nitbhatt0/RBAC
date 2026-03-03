bf973f4ca9c578d48d2e409ef67cdea480855257
nitbhatt0 <91825953+nitbhatt0@users.noreply.github.com>
2026-03-03 17:45:09 +0530

    Update Run-Script.ps1

--- Run-Script.ps1
+++ Run-Script.ps1
@@ -1,10 +1,14 @@
 .\Get-RoleAssignments.ps1 `
  -SentinelWorkspaces @(
-      @{ WorkspaceId = "b7aa54dd-970d-4c78-8f56-5ab1942884c3"; ResourceGroup = "nit-rg1";  SubscriptionId = "f07ec78d-739f-40a0-bcbc-d71385becc02" },
-      @{ WorkspaceId = "e50e8f96-5857-4d26-b09f-b3e84a3cd865"; ResourceGroup = "nit-rg1";   SubscriptionId = "f07ec78d-739f-40a0-bcbc-d71385becc02" } 
+      @{ WorkspaceId = "x-4c78-8f56-5ab1942884c3"; ResourceGroup = "xx";  SubscriptionId = "xxxx" },
+      @{ WorkspaceId = "x-4d26-b09f-b3e84a3cd865"; ResourceGroup = "x";   SubscriptionId = "xxxx" } 
 ) `
   -IncludePIM `
+  -IncludeXDRRBAC `
   -ScanDefenderForCloud `
   -IncludePurview `
-  -PurviewAdminUPN "admin@MngEnvMCAP107482.onmicrosoft.com" `
+  -PurviewAdminUPN "xx" `
+  -AppClientId     "bxx3" `
+  -AppClientSecret "xx" `
+  -AppTenantId     "xx" `
   -ExportFormat Both
